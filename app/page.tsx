01adb286efcaeee583063e57fe3a2bf329619446
anonymous298 <tackletalha@gmail.com>
2025-11-22 21:03:13 +0500

    page.jsx file components placement completed all posts component and suggested user component added

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+// "use client"
+
+import AllPosts from "@/components/Home/AllPosts";
+import SuggestedUsers from "@/components/Home/SuggestedUsers";
+import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/Home/CreatePost";
+
+
+export default async function Home() {
+  const user = await currentUser();
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? 
+        <CreatePost/>
+        :
+        <p>Sign In To see the content</p>}
+
+        <AllPosts/>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4">
+        <SuggestedUsers/>
+      </div>
+    </div>
+  );
+}
