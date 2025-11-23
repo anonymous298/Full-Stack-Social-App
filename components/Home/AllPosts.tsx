f5c984940797f513afb03cba0623236dc20809b8
anonymous298 <tackletalha@gmail.com>
2025-11-23 20:43:11 +0500

    allposts component created and updaetd

--- components/Home/AllPosts.tsx
+++ components/Home/AllPosts.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import PostCard from './PostCard';
+import { getPosts } from '@/server/actions/post.action';
+import { getDbUserId } from '@/server/actions/user.action';
 
-const AllPosts = () => {
+const AllPosts = async () => {
+
+  const posts = await getPosts();
+  const currentUserDbId = await getDbUserId();
+
   return (
-    <div>AllPosts</div>
+    <div>
+      {posts.map((post) => {
+        return <PostCard key={post.id} post={post} dbUserId={currentUserDbId}/>
+      })}
+    </div>
   )
 }
 
