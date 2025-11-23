dbd7181546c8ddbe575ed6b8813b7cfbc29111a9
anonymous298 <tackletalha@gmail.com>
2025-11-23 20:45:02 +0500

    page.jsx page updated

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,13 @@ export default async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? 
-        <CreatePost/>
+        <>
+          <CreatePost/>
+          <AllPosts/>
+        </>
         :
         <p>Sign In To see the content</p>}
 
-        <AllPosts/>
       </div>
 
       <div className="hidden lg:block lg:col-span-4">
